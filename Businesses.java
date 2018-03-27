fe0cf89f1b16fee99ded14f5243deccaa0eb5c37
Mark Nguyen <mark.nguyen.9614@gmail.com>
2018-03-27 14:07:13 +1100

    Draft of Businesses Class

--- Businesses.java
+++ Businesses.java
@@ -1,19 +1,60 @@
 import java.io.*;
 
 public class Businesses implements Serializable{
-	private String buinessName, status, regDate, cancelDate, renewDate, stateNum, regState, abn;
-	private int ID;
+	private String businessName, regDate, cancelDate, renewDate, stateNum, regState, abn;
+	private boolean status;
 
-	public Businesses(int ID, String buinessName, String status, String regDate, String cancelDate, String renewDate, String stateNum, String regState, String abn){
-		this.ID = ID;
-		this.buinessName = buinessName;
+	public Businesses(String businessName, Boolean status, String regDate){
+		this.businessName = businessName;
 		this.status = status;
 		this.regDate = regDate;
+	}
+
+	public String getBusinessName(){return businessName;}
+
+	public boolean getStatus(){return status;}
+
+	public String getRegDate(){return regDate;}
+
+	public String getCancelDate(){return cancelDate;}
+
+	public String getRenewDate(){return renewDate;}
+
+	public String getStateNum(){return stateNum;}
+	
+	public String getRegState(){return regState;}
+
+	public String getAbn(){return abn;}
+
+	public void setBusinessName(String businessName){
+		this.businessName = businessName;
+	}
+
+	public void setStatus(Boolean status){
+		this.status = status;
+	}
+
+	public void setRegDate(String regDate){
+		this.regDate = regDate;
+	}
+
+	public void setcancelDate(String cancelDate){
 		this.cancelDate = cancelDate;
+	}
+
+	public void setRenewDate(String renewDate){
 		this.renewDate = renewDate;
+	}
+
+	public void setStateNum(String stateNum){
 		this.stateNum = stateNum;
+	}
+
+	public void setRegState(String regState){
 		this.regState = regState;
+	}
+
+	public void setAbn(String abn){
 		this.abn = abn;
 	}
-	
 }
